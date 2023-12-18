7fac7f6a16a0971c8ab7413d61d08b8a0e2d27c1
SammedCH <sammedhuchchannavar1308@gmail.com>
2023-12-18 11:15:48 +0530

    var type can store any dataType in it.

---
+++ varType/dynamic/A.java
@@ -0,0 +1,18 @@
+package varType.dynamic;
+
+public class A {
+
+	public static void main(String[] args) {
+       var a=10;
+       var b="String";
+       var c='C';
+       var d=true;
+       var e=10.5;
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(d);
+       System.out.println(e);
+	}
+
+}
---
+++ variableAndKeyword/A.java
@@ -0,0 +1,8 @@
+package variableAndKeyword;
+
+public class A {
+public static void main(String[] args) {
+	var var=10;
+	System.out.println(var);
+}
+}
